7a72dfd93f7d501e85975d4fed90e304e7ecf6c7
joao.victor <joao.victor@f13.com.br>
2020-07-27 11:31:43 -0300

    fix responseEntity

--- src/main/java/com/example/carros/controller/CarrosController.java
+++ src/main/java/com/example/carros/controller/CarrosController.java
@@ -3,32 +3,56 @@ package com.example.carros.controller;
 import com.example.carros.domain.model.Carro;
 import com.example.carros.domain.usecase.CarroBusiness;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/carros")
 public class CarrosController {
 
     @Autowired
     private CarroBusiness carroBusiness;
 
-    @GetMapping(path = "/carros")
-    public List<Carro> listAll() {
-        return  this.carroBusiness.findAll();
+    @GetMapping()
+    public ResponseEntity<List<Carro>> listAll() {
+        return ResponseEntity.ok(this.carroBusiness.findAll());
     }
 
-    @GetMapping(path = "/carros/{id}")
-    public Optional<Carro> findById(@PathVariable Integer id) {
-        return this.carroBusiness.findById(id);
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Carro> findById(@PathVariable Integer id) {
+        Optional<Carro> carroOptional = this.carroBusiness.findById(id);
+
+        return carroOptional
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping(path = "/carros/tipo/{tipo}")
-    public List<Carro> findByTipo(@PathVariable() String tipo) {
-        return this.carroBusiness.findByTipo(tipo);
+    @GetMapping(path = "/tipo/{tipo}")
+    public ResponseEntity<List<Carro>> findByTipo(@PathVariable() String tipo) {
+        List<Carro> carroList = this.carroBusiness.findByTipo(tipo);
+
+        return carroList.isEmpty() ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.ok(carroList);
+    }
+
+    @PostMapping()
+    public Carro save(@RequestBody Carro carro) {
+        return this.carroBusiness.save(carro);
+    }
+
+    @PutMapping()
+    public Carro update(@RequestBody Carro carro) {
+        return this.carroBusiness.update(carro);
+    }
+
+    @DeleteMapping()
+    public void delete(@RequestBody Carro carro) {
+        this.carroBusiness.delete(carro);
     }
 
 }
--- src/main/java/com/example/carros/domain/usecase/CarroBusiness.java
+++ src/main/java/com/example/carros/domain/usecase/CarroBusiness.java
@@ -26,4 +26,32 @@ public class CarroBusiness {
         return this.carroRepository.findByTipo(tipo);
     }
 
+    public Carro save(Carro carro) {
+        return this.carroRepository.save(carro);
+    }
+
+    public Carro update(Carro carro) {
+
+        Optional<Carro> carroOptional = findById(carro.getId());
+
+        if (carroOptional.isPresent()) {
+            Carro carroMock = carroOptional.get();
+            carroMock.setNome(carro.getNome());
+            carroMock.setTipo(carro.getTipo());
+
+            return this.carroRepository.save(carroMock);
+        } else {
+            throw new RuntimeException("Não foi possível atualizar o registro");
+        }
+    }
+
+    public void delete(Carro carro) {
+
+        Optional<Carro> carroOptional = findById(carro.getId());
+
+        if (carroOptional.isPresent()) {
+            this.carroRepository.delete(carro);
+        }
+    }
+
 }
